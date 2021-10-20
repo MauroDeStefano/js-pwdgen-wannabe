9ac91eff13d9d055c98954097af67840dbcdad7b
MauroDeStefano <maurodestefano@gmail.com>
2021-10-20 13:58:00 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const userFirstName= prompt("Inserisci il tuo nome");
+const userLastName= prompt("Inserisci il cognome");
+const favColour= prompt("Inserisci il colore preferito") ;
+
+const passwordGenerated = userFirstName + userLastName + favColour;
+
