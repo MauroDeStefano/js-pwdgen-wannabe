f13600c238b1ccf64fcfaa889241f81e829ac04e
MauroDeStefano <maurodestefano@gmail.com>
2021-10-20 14:34:51 +0200

    Added password generator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-const userFirstName= prompt("Inserisci il tuo nome");
+dconst userFirstName= prompt("Inserisci il tuo nome");
 const userLastName= prompt("Inserisci il cognome");
 const favColour= prompt("Inserisci il colore preferito") ;
+const numberRandom= Math.floor(Math.random() * 99);
 
-const passwordGenerated = userFirstName + userLastName + favColour;
+
+const passwordGenerated = userFirstName + userLastName + favColour + numberRandom;
+
+
+document.getElementById("newPass").innerHTML=
+`
+  Nome: <strong>${userFirstName} ${userLastName}</strong><br>
+  <br>
+  Password: ${passwordGenerated}<br>
+  <br>
+  <h3 class="text-danger pb-5">PLEASE KEEP YOUR PASSWORD SAFE</h3>
+
+  Lunghezza password = ${passwordGenerated.length}
+`
+;
 
